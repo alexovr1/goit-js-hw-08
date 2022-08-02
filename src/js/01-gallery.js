1403ec6e5c5c918b6a0b26ee45db28da11546e0c
Oleksandr Ovrakh <oleksandr.ovrakh@gmail.com>
2022-08-02 23:09:10 +0300

    task 1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,34 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+const refs = {
+    gallery: document.querySelector('.gallery'),
+    body: document.querySelector('body')
+}
+
+// -----Create DOM-elements in memory-----
+const galleryItemsRef = galleryItems.map(({ preview, original, description }) => `<div class="gallery">
+    <a class="gallery__item" href="${original}" onclick="return false;">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+        />
+    </a>
+</div>`).join("");
+
+
+// -----Create DOM-elements in HTML-----
+refs.gallery.innerHTML = galleryItemsRef;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionsDelay: 250,
+    disableRightClick: true,
+    overlayOpacity: 0.8,
+});
