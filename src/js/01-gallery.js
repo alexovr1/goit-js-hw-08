dae28ac751e67811f9af59dba416a5f4ca5b595c
Oleksandr Ovrakh <oleksandr.ovrakh@gmail.com>
2022-08-04 22:58:55 +0300

    up

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,4 @@
+import galleryItemsTpl from '../templates/gallery-items.hbs';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
@@ -12,19 +13,22 @@ const refs = {
 }
 
 // -----Create DOM-elements in memory-----
-const galleryItemsRef = galleryItems.map(({ preview, original, description }) => `<div class="gallery">
-    <a class="gallery__item" href="${original}" onclick="return false;">
-        <img
-            class="gallery__image"
-            src="${preview}"
-            alt="${description}"
-        />
-    </a>
-</div>`).join("");
+// const galleryItemsRef = galleryItems.map(({ preview, original, description }) => `<div class="gallery">
+//     <a class="gallery__item" href="${original}" onclick="return false;">
+//         <img
+//             class="gallery__image"
+//             src="${preview}"
+//             alt="${description}"
+//         />
+//     </a>
+// </div>`).join("");
 
 
 // -----Create DOM-elements in HTML-----
-refs.gallery.innerHTML = galleryItemsRef;
+// refs.gallery.innerHTML = galleryItemsRef;
+refs.gallery.innerHTML = galleryItemsTpl(galleryItems);
+
+
 
 const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
